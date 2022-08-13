538cb181d53c49d6892a36bc9d328d764185fc60
chenx23333@gmail.com <chenx23333@gmail.com>
2022-08-13 15:44:15 -0400

    Comment out one of the environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,15 +12,15 @@ export const environment = {
   },
 };
 
-export const environment = {
-  production: true,
-  withCredentials: true,
-  baseUrl: "http://backend-env.eba-g9uchpeu.us-west-2.elasticbeanstalk.com",
-  headers: {
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*',
-  }
-}
+// export const environment = {
+//   production: true,
+//   withCredentials: true,
+//   baseUrl: "http://backend-env.eba-g9uchpeu.us-west-2.elasticbeanstalk.com",
+//   headers: {
+//     'Content-Type': 'application/json',
+//     'Access-Control-Allow-Origin': '*',
+//   }
+// }
 
 
 
